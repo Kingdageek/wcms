a552c3209bb6547766b99c02c07e24682cd7a600
Nonso Mgbechi <mgbechinonso@gmail.com>
2018-11-20 23:49:51 +0100

    Added leaderBoxJs function to show leader-box if member is a worker

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,16 @@ $(".sl").on('click', function() {
     // FadeIn a loader modal
     $('.ajload').show();
 });
+
+$('#close-rpt').on('click', function() {
+    $('#report-house').fadeOut();
+});
+
+function leaderBoxJs() {
+    let isworker = $("#isworker").val()
+    if (isworker == "Yes") {
+        $("#leader-box").fadeIn();
+    } else if (isworker == "No") {
+        $("#leader-box").fadeOut();
+    }
+}
