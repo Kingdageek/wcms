d0424d4f3c2cec064a6610ce995a07412ba94872
Nonso Mgbechi <mgbechinonso@gmail.com>
2018-11-27 23:11:35 +0100

    Moved the displayPage function to helpers

--- index.php
+++ index.php
@@ -6,19 +6,8 @@
     } else {
         $username = $_SESSION['username'];
         include_once "views/partials/navbar.html.php";
-        $url = $_GET['page'] ?? '';
+        $url = $_GET['page'] ?? '';     
         displayPage($url);
-    }
-
-    function displayPage($url)
-    {
-        $url = htmlspecialchars($url, ENT_QUOTES,  'UTF-8');
-        $routes = require_once "routes.php";
-        if (array_key_exists($url, $routes)) {
-            include_once $routes[$url];
-        } else {
-            echo "Page not found! I want to believe you're not messing with my url on purpose Zig!";
-        }
     }
     include_once "views/partials/footer.html.php"; 
 ?>
